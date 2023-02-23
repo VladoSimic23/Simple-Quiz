a5759d3c52c83e17c142e632c97ee20e035de98d
VladoSimic23 <57799129+VladoSimic23@users.noreply.github.com>
2023-02-23 13:32:07 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,9 +13,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-// apiKey: process.env.REACT_APP_API_KEY,
-// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-// projectId: process.env.REACT_APP_PROJECT_ID,
-// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-// appId: process.env.REACT_APP_APP_ID,
