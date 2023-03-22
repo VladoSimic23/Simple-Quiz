304f705d1d94bb5e1c1d7d2d65fe7b238edf741f
VladoSimic23 <57799129+VladoSimic23@users.noreply.github.com>
2023-03-22 11:50:45 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,7 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, {
-  experimentalForceLongPolling: true,
-});
+export const db = getFirestore(app);
 
